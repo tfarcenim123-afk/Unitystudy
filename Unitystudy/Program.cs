9bdd1070fa0400034b87cc882b6b83d27172eec1
tfarcenim123-afk <tfarcenim123@gmail.com>
2025-11-24 16:33:26 +0900

    gugudan(for study)

--- Unitystudy/Program.cs
+++ Unitystudy/Program.cs
@@ -4,13 +4,18 @@
     {
         static void Main(string[] args)
         {
-            int num = 1;
+            //int num = 1;
 
-            while (num <= 10)
+            //while (num <= 10)
+            //{
+            //    Console.WriteLine(num);
+            //    num++;
+            //}
+
+            for (int n = 0; n < 9; n++)
             {
-                Console.WriteLine(num);
-                num++;
-            }
+                Console.WriteLine(n);
+            } 
         }
     }
 }
---
+++ gugudan/Program.cs
@@ -0,0 +1,14 @@
+﻿    internal class Program
+    {
+        static void Main()
+        {
+            for (int n = 2; n <= 9; n++)
+            {
+                Console.WriteLine($"{n}단");
+                for (int m = 1; m <= 9; m++)
+                {
+                    Console.WriteLine($"{n} x {m} = {n * m}");
+                }
+            }
+        }
+    }
