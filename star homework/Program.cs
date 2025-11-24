4fabed9bc3d9390a2ee7d94c997c0e324285cfab
tfarcenim123-afk <tfarcenim123@gmail.com>
2025-11-24 17:39:38 +0900

    star homework

---
+++ star homework/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+    {
+        static void Main()
+        {
+            for (int m = 1; m <= 5; m++)
+            {
+                for (int n = 1; n <= m; n++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int m = 1; m <= 5; m++)
+            {
+                for (int n = 5; n >= m; n--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int n = 1; n <= 10; n++)
+            {
+                if (n % 2 == 1)
+                    Console.WriteLine($"{n}은 홀수입니다.");
+                else
+                    Console.WriteLine($"{n}은 짝수입니다.");
+            }
+        }
+    }
