ab07a532bebcd95cc2cfdcea36d7dc061f041f39
tfarcenim123-afk <tfarcenim123@gmail.com>
2025-11-25 09:24:33 +0900

    sum of 1 to 10

---
+++ sumof1to10/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+    {
+        static void Main()
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                sum = sum + i;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+
