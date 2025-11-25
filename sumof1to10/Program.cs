37e4b1259039fed45c3a49536eb9b1a2be942622
tfarcenim123-afk <tfarcenim123@gmail.com>
2025-11-25 10:42:40 +0900

    test

--- sumof1to10/Program.cs
+++ sumof1to10/Program.cs
@@ -1,7 +1,16 @@
 ﻿internal class Program
     {
+        static void Output(int a, int b) //함수 선언
+        {
+            Console.WriteLine(a+b);
+        }
         static void Main()
         {
+            int a = 10;
+            int b = 20;
+        
+            Output(a,b); //함수 호출
+
             int sum = 0;
 
             for (int i = 1; i <= 10; i++)
@@ -9,6 +18,8 @@
                 sum = sum + i;
                 Console.WriteLine(sum);
             }
-        }
+
+
     }
+}
 
---
+++ test/Program.cs
@@ -0,0 +1,12 @@
+﻿    internal class Program
+    {
+        static int Test(int a)
+        {
+            a++;
+            return a;
+        }
+        static void Main()
+        {
+            Test(10);
+        }
+    }
