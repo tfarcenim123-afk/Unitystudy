7a8c4a608ec783f9e07946307d161666f67c0abc
tfarcenim123-afk <tfarcenim123@gmail.com>
2025-12-03 10:59:12 +0900

    study 1203

---
+++ FunctionPractice/FunctionPractice/Program.cs
@@ -0,0 +1,36 @@
+﻿    internal class Program
+    {
+        //static void Sum(int start, int end)
+        //{
+        //    int sum = 0;
+
+        //    for (int i = start; i <= end; i++)
+        //    {
+        //        sum += i;
+        //    }
+
+        //    Console.WriteLine(sum);
+        //}
+
+        //static void Main()
+        //{
+        //    Sum(1, 6);
+        //}
+        static int Sum(int start, int end)
+        {
+            int sum = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+        static void Main()
+        {
+            int sum = Sum(1, 6);
+
+            Console.WriteLine(sum);
+        }
+    }
