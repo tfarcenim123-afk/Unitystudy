ae92a154a46ceb33a9da22c8c54aa44c82878b43
tfarcenim123-afk <tfarcenim123@gmail.com>
2025-12-03 19:55:39 +0900

    homework

---
+++ SimpleRPG/SimpleRPG/Program.cs
@@ -0,0 +1,160 @@
+﻿class Program
+{
+    class Character
+    {
+        public Character()
+        {
+            
+        }
+
+        public string name = string.Empty;
+        public int hp;
+        public int atk;
+
+        //A가 공격하면 B의 체력을 A의 공격력만큼 감소시킴
+        public void Attack(Character target)
+        {
+            Console.WriteLine($"{this.name}의 공격! {target.name}에게 {this.atk}만큼 피해를 주었다.");
+            Console.WriteLine();
+
+            target.hp = Math.Max(target.hp - this.atk, 0);
+        }
+
+        //캐릭터의 hp가 0이면 사망 판정
+        public bool IsDead()
+        {
+            return this.hp == 0;
+        }
+    }
+
+    class Player : Character
+    {
+        public Player(string name) : base()
+        {
+            this.name = name;
+            hp = 1000;
+            atk = 10;
+        }
+
+        public int playerExp = 0;
+        public int expAmount = 10;
+
+        public void GainExp()
+        {
+            playerExp += expAmount;
+
+            Console.WriteLine($"{expAmount} 경험치를 얻었습니다.");
+            Console.WriteLine();
+        }
+    }
+
+    class Monster : Character
+    {
+        //몬스터 이름 랜덤으로 정해주기
+        static string RandomName()
+        {
+            string[] names = { "슬라임", "고블린", "늑대", "박쥐" };
+            return names[new Random().Next(names.Length)];
+        }
+
+        Random monsterHp = new Random();
+        Random monsterAtk = new Random();
+
+        public Monster() : base()
+        {
+            name = RandomName();
+            hp = monsterHp.Next(20, 51);
+            atk = monsterAtk.Next(2, 7);
+        }
+    }
+
+    static void Main()
+    {
+        //초기화
+        bool isGameOver = false;
+
+        Console.Clear();
+        Console.Title = "Console Dungeon";
+
+        //플레이어 이름 입력 받기
+        Console.Write("이름을 입력하세요: ");
+        
+        //플레이어 생성
+        Player player = new Player(Console.ReadLine());
+
+        //게임 루프
+        while (!isGameOver)
+        {
+            //몬스터 스폰
+            Monster monster = new Monster();
+
+            //플레이어 상태 창
+            Console.WriteLine("================================================");
+            Console.WriteLine($"| {player.name} | HP: {player.hp} | ATK: {player.atk} | EXP: {player.playerExp} |");
+
+            //몬스터 상태 창
+            Console.WriteLine("================================================");
+            Console.WriteLine($"| {monster.name} | HP: {monster.hp} | ATK: {monster.atk} |");
+
+            //플레이어가 게임을 계속하고 싶은지 확인
+            Console.WriteLine("================================================");
+            Console.Write("계속 싸우시겠습니까? ( Y : 예 / N : 아니오 )  ");
+
+            //플레이어가 유효한 키를 입력했는지
+            bool isValidKey = false;
+
+            while (!isValidKey)
+            {
+                //플레이어 키 입력 감지
+                ConsoleKeyInfo playerInput = Console.ReadKey();
+
+                switch (playerInput.Key)
+                {
+                    //Y 입력시
+                    case ConsoleKey.Y:
+                        isValidKey = true;
+                        isGameOver = false;
+                        break;
+
+                    //N 입력시
+                    case ConsoleKey.N:
+                        isValidKey = true;
+                        isGameOver = true;
+                        break;
+
+                    //유효하지 않은 키 입력 시
+                    default:
+                        break;
+                }
+            }
+
+            //줄 띄우기
+            Console.WriteLine();
+            Console.WriteLine("================================================");
+            Console.WriteLine();
+
+            //플레이어가 몬스터를 공격
+            player.Attack(monster);
+
+            //몬스터 사망 판정, 사망했다면 플레이어는 경험치 획득
+            if (monster.IsDead())
+            {
+                player.GainExp();
+            }
+
+            //몬스터가 플레이어를 공격
+            monster.Attack(player);
+
+            //플레이어 사망 판정, 사망했다면 게임 종료
+            if (player.IsDead())
+            {
+                isGameOver = true;
+                break;
+            }
+        }
+
+        //게임 종료
+        Console.Clear();
+        Console.WriteLine("GAME OVER");
+    }
+}
