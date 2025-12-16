fdaaa3c9cbf9c4dc87cdd328b3e5f7a965fcaef3
tfarcenim123-afk <tfarcenim123@gmail.com>
2025-12-16 16:58:09 +0900

    feat: stack implement

---
+++ StackPractice/StackPractice/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Text;
+
+internal class Program
+{
+    //정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+
+    //명령은 총 다섯 가지이다.
+
+    public static List<int> stack = new List<int>();
+
+    public static int input = 0;
+
+    public static StringBuilder sb;
+    static void Main()
+    {
+        input = int.Parse(Console.ReadLine());
+
+        sb = new StringBuilder(input);
+
+        for (int i = 0; i < input; i++)
+        {
+            string what = Console.ReadLine();
+
+            switch (what)
+            {
+                case "pop":
+                    {
+                        Pop();
+                    }
+                    break;
+
+                case "size":
+                    {
+                        Size();
+                    }
+                    break;
+
+                case "empty":
+                    {
+                        Empty();
+                    }
+                    break;
+
+                case "top":
+                    {
+                        Top();
+                    }
+                    break;
+
+                default:
+                    if (what.Contains("push"))
+                    {
+                        Push(int.Parse(what.Split()[1]));
+                    }
+                    break;
+            }
+        }
+
+        Console.WriteLine(sb);
+    }
+
+    /// <summary>
+    /// push X: 정수 X를 스택에 넣는 연산이다.
+    /// </summary>
+    static void Push(int X)
+    {
+        stack.Add(X);
+    }
+
+    /// <summary>
+    /// pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 
+    ///     만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+    /// </summary>
+    static void Pop()
+    {
+        int pop = stack.Count - 1;
+
+        if (pop < 0)
+        {
+            sb.AppendLine("-1");
+            return;
+        }
+
+        sb.AppendLine($"{stack[pop]}");
+
+        stack.RemoveAt(pop);
+    }
+
+    /// <summary>
+    /// size: 스택에 들어있는 정수의 개수를 출력한다.
+    /// </summary>
+    static void Size()
+    {
+        sb.AppendLine($"{stack.Count}");
+    }
+
+    /// <summary>
+    /// empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
+    /// </summary>
+    static void Empty()
+    {
+        if (stack.Count == 0) sb.AppendLine("1");
+        else sb.AppendLine("0");
+    }
+
+    /// <summary>
+    /// top: 스택의 가장 위에 있는 정수를 출력한다. 
+    ///     만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+    /// </summary>
+    static void Top()
+    {
+        int top = stack.Count - 1;
+
+        if (top < 0)
+        {
+            sb.AppendLine("-1");
+            return;
+        }
+
+        sb.AppendLine($"{stack[top]}");
+    }
+}
---
+++ sjsj1216/sjsj1216/Program.cs
@@ -0,0 +1,151 @@
+﻿using System.Text;
+        internal class Program
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine()); // N 입력
+
+            string[] arrayN = Console.ReadLine().Split(); // 크기가 N인 정수 배열
+
+            int M = int.Parse(Console.ReadLine()); // M 입력
+
+            string[] arrayM = Console.ReadLine().Split(); // 크기가 M인 정수 배열
+
+            Array.Sort(arrayN);
+
+            StringBuilder sb = new StringBuilder(N);
+
+            foreach (string m in arrayM)
+            {
+                if (Array.BinarySearch(arrayN, m) > -1)
+                {
+                    sb.AppendLine("1");
+                    continue;
+                }
+
+                sb.AppendLine("0");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+
+    //int N = int.Parse(Console.ReadLine()); // N 입력
+
+    //    string[] arrayN = Console.ReadLine().Split(); // 크기가 N인 정수 배열
+
+    //    for(int i = 0; i < 10; i++)
+    //    {
+
+    //    }
+    //    int[] groupA = new int[N];  //log31~30
+    //    int[] groupB = new int[N];  //log30~28
+    //    int[] groupC = new int[N];  //log28~24
+    //    int[] groupD = new int[N];  //log24~16
+    //    int[] groupE = new int[N];  //log16~00
+    //    int[] groupF = new int[N];  //log00~16
+    //    int[] groupG = new int[N];  //log16~24
+    //    int[] groupH = new int[N];  //log24~28
+    //    int[] groupI = new int[N];  //log28~30
+    //    int[] groupJ = new int[N];  //log30~31
+
+    //    int M = int.Parse(Console.ReadLine()); // M 입력
+
+    //    string[] arrayM = Console.ReadLine().Split(); // 크기가 M인 정수 배열
+
+        //List<int> list = Console.ReadLine()!.Split().Select(int.Parse).ToList();
+
+//bool[] isThereSameNumberPstv1 = new bool[int.MaxValue / 2 + 1]; // 0부터 2^30-1 까지
+//bool[] isThereSameNumberPstv2 = new bool[int.MaxValue / 2 + 1]; // 2^30 부터 2^31-1 까지
+//bool[] isThereSameNumberNgtv1 = new bool[int.MaxValue / 2 + 1]; // -1부터 -2^30 까지
+//bool[] isThereSameNumberNgtv2 = new bool[int.MaxValue / 2 + 1]; // -2^30-1부터 -2^31까지
+
+//foreach (string n in arrayN)
+//{
+//    int i = int.Parse(n);
+
+//    if (0 <= i && i <= int.MaxValue / 2)
+//    {
+//        isThereSameNumberPstv1[i] = true;
+//    }
+//    if (int.MaxValue / 2 < i)
+//    {
+//        isThereSameNumberPstv2[i - (int.MaxValue / 2) - 1] = true;
+//    }
+//    if (-(int.MaxValue / 2) - 1 <= i && i < 0)
+//    {
+//        isThereSameNumberNgtv1[-i - 1] = true;
+//    }
+//    if (i < -(int.MaxValue / 2) - 1)
+//    {
+//        isThereSameNumberNgtv2[-i - (int.MaxValue / 2) + 2] = true;
+//    }
+//}
+
+//int M = int.Parse(Console.ReadLine()); // M 입력
+
+//string[] arrayM = Console.ReadLine().Split(); // 크기가 M인 정수 배열
+
+//foreach (string m in arrayM)
+//{
+//    int i = int.Parse(m);
+
+//    if (0 <= i && i <= int.MaxValue / 2)
+//    {
+//        if (isThereSameNumberPstv1[i])
+//        {
+//            Console.WriteLine(1);
+//            continue;
+//        }
+//    }
+//    if (int.MaxValue / 2 < i)
+//    {
+//        if (isThereSameNumberPstv2[i - (int.MaxValue / 2) - 1])
+//        {
+//            Console.WriteLine(1);
+//            continue;
+//        }
+//    }
+//    if (-(int.MaxValue / 2) - 1 <= i && i < 0)
+//    {
+//        if (isThereSameNumberNgtv1[-i - 1])
+//        {
+//            Console.WriteLine(1);
+//            continue;
+//        }
+//    }
+//    if (i < -(int.MaxValue / 2) - 1)
+//    {
+//        if (isThereSameNumberNgtv2[-i - (int.MaxValue / 2) + 2])
+//        {
+//            Console.WriteLine(1);
+//            continue;
+//        }
+//    }
+
+//    else
+//    {
+//        Console.WriteLine(0);
+//    }
+//}
+
+//int N = int.Parse(Console.ReadLine()); // N 입력
+
+//string[] arrayN = Console.ReadLine().Split(); // 크기가 N인 정수 배열
+
+//int M = int.Parse(Console.ReadLine()); // M 입력
+
+//string[] arrayM = Console.ReadLine().Split(); // 크기가 M인 정수 배열
+
+//Array.Sort(arrayN); //O(NlogN)
+
+//foreach (string m in arrayM)
+//{
+//    if (Array.BinarySearch(arrayN, m) > -1)
+//    {
+//        Console.WriteLine(1);
+//        continue;
+//    }
+
+//    Console.WriteLine(0);
+//}
