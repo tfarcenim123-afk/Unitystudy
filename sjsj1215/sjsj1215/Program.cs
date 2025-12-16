ace70da5362e09cf579b4bd213f07e9178f9813f
tfarcenim123-afk <tfarcenim123@gmail.com>
2025-12-16 10:21:11 +0900

    feat : change to O(logN)

--- sjsj1215/sjsj1215/Program.cs
+++ sjsj1215/sjsj1215/Program.cs
@@ -2,14 +2,33 @@
 {
     static void Main()
     {
-        List<int> list = new List<int> { 1, 2, 4, 8, 16, 32, 64 };
+        List<int> list = new List<int>();
 
+        Random random = new Random();
+
+        int number = 0;
+
+        for (int i = 0; i < random.Next(10, 20); i++)
+        {
+            number += random.Next(1, 10);
+
+            list.Add(number);
+
+            Console.Write(number + " ");
+        }
+
+        Console.WriteLine();
+
+        Console.Write("Target number : ");
         int input = int.Parse(Console.ReadLine());
 
+        Console.Write("Is there target number? : ");
         Console.WriteLine(BinarySearch(list, input));
 
+        Console.Write("Index of Lower Bound is ");
         Console.WriteLine(LowerBound(list, input));
 
+        Console.Write("Index of Upper Bound is ");
         Console.WriteLine(UpperBound(list, input));
     }
 
@@ -18,25 +37,23 @@
     ///</summary>
     static bool BinarySearch(List<int> list, int key)
     {
-        if (list[list.Count / 2] == key)
-        {
-            return true;
-        }
+        int min = 0;
+        int max = list.Count;
+        int mid;
 
-        else if (list.Count / 2 > 0)
+        while (min < max)
         {
-            if (list[list.Count / 2] > key)
+            mid = (min + max)/2;
+
+            if (list[mid] == key) return true;
+
+            if (list[mid] > key) //key가 중앙값보다 왼쪽에 있다면
             {
-                List<int> nextList = new List<int>();
-                nextList = list.GetRange(0, list.Count / 2);
-                return BinarySearch(nextList, key);
+                max = mid;
             }
-
-            if (list[list.Count / 2] < key)
+            else //key가 중앙값보다 오른쪽에 있다면
             {
-                List<int> nextList = new List<int>();
-                nextList = list.GetRange(list.Count / 2 + 1, (list.Count - 1) / 2);
-                return BinarySearch(nextList, key);
+                min = mid + 1;
             }
         }
 
@@ -48,15 +65,27 @@
     ///</summary>
     static int LowerBound(List<int> list, int key)
     {
-        foreach (int i in list)
+        int min = 0;
+        int max = list.Count;
+        int mid;
+        int result = list.Count;
+
+        while (min < max) //리스트의 크기가 2이상이면 반복
         {
-            if(i >= key)
+            mid = (min + max) / 2; //mid 인덱스 구하기
+
+            if (list[mid] < key) //key 값이 mid보다 크면
             {
-                return list.IndexOf(i);
+                min = mid + 1; //mid 오른쪽 다시 탐색
+            }
+            else //key 값이 mid보다 작거나 같으면
+            {
+                result = mid; //mid 값이 정답일 수도 있고
+                max = mid; //mid 왼쪽 다시 탐색
             }
         }
 
-        return -1;
+        return result;
     }
 
     ///<summary>
@@ -64,14 +93,79 @@
     ///</summary>
     static int UpperBound(List<int> list, int key)
     {
-        foreach (int i in list)
+        int min = 0;
+        int max = list.Count;
+        int mid;
+        int result = list.Count;
+
+        while (min < max) //리스트의 크기가 2이상이면 반복
         {
-            if (i > key)
+            mid = (min + max) / 2; //mid 인덱스 구하기
+
+            if (list[mid] > key) //key 값이 mid보다 작으면
             {
-                return list.IndexOf(i);
+                result = mid; //mid 값이 정답일 수도 있고
+                max = mid; //mid 왼쪽 다시 탐색
+            }
+            else //key 값이 mid보다 크거나 같으면
+            {
+                min = mid + 1; //mid 오른쪽 다시 탐색
             }
         }
 
-        return -1;
+        return result;
     }
 }
+
+//static int LowerBound(List<int> list, int key)
+//{
+//    foreach (int i in list)
+//    {
+//        if(i >= key)
+//        {
+//            return list.IndexOf(i);
+//        }
+//    }
+
+//    return -1;
+//}
+
+//static int UpperBound(List<int> list, int key)
+//{
+//    foreach (int i in list)
+//    {
+//        if (i > key)
+//        {
+//            return list.IndexOf(i);
+//        }
+//    }
+
+//    return -1;
+//}
+
+//static bool BinarySearch(List<int> list, int key)
+//{
+//    if (list[list.Count / 2] == key)
+//    {
+//        return true;
+//    }
+
+//    else if (list.Count / 2 > 0)
+//    {
+//        if (list[list.Count / 2] > key)
+//        {
+//            List<int> nextList = new List<int>();
+//            nextList = list.GetRange(0, list.Count / 2);
+//            return BinarySearch(nextList, key);
+//        }
+
+//        if (list[list.Count / 2] < key)
+//        {
+//            List<int> nextList = new List<int>();
+//            nextList = list.GetRange(list.Count / 2 + 1, (list.Count - 1) / 2);
+//            return BinarySearch(nextList, key);
+//        }
+//    }
+
+//    return false;
+//}
